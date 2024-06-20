5923b5e14ccf48893fd587665cd8bb3b7f1eb11d
VespaV <maria27sir@gmail.com>
2024-06-20 18:22:27 +0200

    blast fix (path)

--- homologous_sequence/constants.py
+++ homologous_sequence/constants.py
@@ -8,4 +8,4 @@ SCORE_OUTPUT_FOLDER = 'homologous_sequence/static/output_files/score_output_file
 TEMP_FILES_DIR = 'homologous_sequence/static/output_files/temporary_file/'
 BLAST_CVS_FOLDER = 'homologous_sequence/static/output_files/blast_cvs_files/'
 BLAST_XLSX_FOLDER = 'homologous_sequence/static/output_files/blast_excel_files/'
-DB_PROTEIN_FOR_BLAST = 'BLAST/'
+DB_PROTEIN_FOR_BLAST = '../BLAST/'
